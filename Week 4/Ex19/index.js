7c6d3ec8ddcb8c6f0468e769539e74741c2171c8
nhernandezsf <nandez90@gmail.com>
2020-10-20 22:48:00 -0700

    ex19

---
+++ Week 4/Ex19/index.js
@@ -0,0 +1,7 @@
+// DON'T TOUCH THIS LINE! (please)
+const word = 'skateboard' //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+word.slice(5, 11);
+word.replace('o', 'e');
+let facialHair = word.slice(5, 11).replace('o','e');
