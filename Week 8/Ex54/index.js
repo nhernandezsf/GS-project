11f23f6c0ae8041a0d51cf68f7a11a775461e23f
nhernandezsf <nandez90@gmail.com>
2020-10-24 15:56:27 -0700

    ex54

---
+++ Week 8/Ex54/index.js
@@ -0,0 +1,7 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+
+let image = document.querySelector('img')
+
+image.setAttribute('src', 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg')
+
+image.setAttribute('alt', 'chicken')
