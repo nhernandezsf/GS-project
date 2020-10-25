152f71fe51188055660c77edd8f1e9ccad94ff5a
nhernandezsf <nandez90@gmail.com>
2020-10-24 21:44:41 -0700

    Ex55

---
+++ Week 8/Ex55/index.js
@@ -0,0 +1,6 @@
+let container = document.getElementById('container')
+container.style.textAlign = 'center'
+
+let image = document.querySelector('img');
+image.style.width = '150px'
+image.style.borderRadius = '50%'
