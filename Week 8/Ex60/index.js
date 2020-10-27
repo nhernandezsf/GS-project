1872be0f1de71035fecec6956307c63f32a70178
nhernandezsf <nandez90@gmail.com>
2020-10-27 16:34:42 -0700

    ex60

---
+++ Week 8/Ex60/index.js
@@ -0,0 +1,11 @@
+const button1 = document.querySelector('#hello');
+
+button1.addEventListener('click', function () {
+    console.log("hello")
+})
+
+const button2 = document.querySelector('#goodbye');
+
+button2.addEventListener('click', function(){
+    console.log("goodbye")
+})
